41ed835c14e3baa3139039a5553b4f209dedf429
Shreenarayan <127131074+shreenarayan123@users.noreply.github.com>
2025-08-08 13:28:19 +0530

    chatbot ui fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from '@/components/theme-provider'
 import { Analytics } from '@vercel/analytics/next'
-import ChatBotIframe from '@/components/Chatbot'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -28,7 +27,7 @@ export default function RootLayout({
           
           <main>{children}</main>
           <Analytics/>
-          <ChatBotIframe/>
+          {/* <ChatBotIframe/> */}
         </ThemeProvider>
       </body>
     </html>
